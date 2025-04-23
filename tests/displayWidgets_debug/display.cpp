f020fcee5c516f030f323104ebbe6f2142d645f1
CharaVerKys <charaverk@gmail.com>
2025-04-23 17:08:21 +0300

    add debug

---
+++ tests/displayWidgets_debug/display.cpp
@@ -0,0 +1,20 @@
+#include "matchlistwidget.hpp"
+#include <QApplication>
+
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    MatchListWidget w(nullptr);
+    lhc::protocol::payload::listOfAllMatches all;
+    all.vec =  
+    {{figure_side::black, 123123},
+    {figure_side::black, 55555555},
+    {figure_side::white, 132333},};
+    w.initList(all);
+    // w.setContentsMargins(0,0,0,0);
+    w.show();
+    
+
+    return app.exec();
+}
